20a5220920847202777cedcadeccdef94baaf691
Alessio Frittoli <info@alessiofrittoli.it>
2025-03-21 15:52:44 +0100

    tests: improved tests scripts

--- __tests__/check.test.ts
+++ __tests__/check.test.ts
@@ -1,63 +1,10 @@
 /**
  * @jest-environment-options {"url": "http://localhost:3000/"}
  */
-import { isAbsoluteUrl, isExternalUrl } from '@/check'
+import { isAbsoluteUrl } from '@/check'
+import { isExternalUrlUnitTests } from './check'
 
-describe( 'isExternalUrl', () => {
-
-	const isClient		= typeof window !== 'undefined'
-	const clientSuffix	= isClient ? ' in the client' : ''
-	
-	it( 'returns false if no location has been provided', () => {
-		expect( isExternalUrl( new URL( 'https://externalurl.it' ), undefined ) )
-			.toBe( isClient )
-	} )
-
-
-	it( 'checks URL protocol' + clientSuffix, () => {
-		const url1 = new URL( 'http://localhost:3000' )
-		const url2 = new URL( 'https://localhost:3000' )
-
-		expect(
-			isExternalUrl( url1, url2 )
-		).toBe( true )
-
-	} )
-
-
-	it( 'doesn\'t check URL pathname' + clientSuffix, () => {
-		const url1 = new URL( 'http://localhost:3000/pathname' )
-		const url2 = new URL( 'http://localhost:3000/different-pathname' )
-
-		expect(
-			isExternalUrl( url1, url2 )
-		).toBe( false )
-
-	} )
-
-
-	it( 'checks URL port' + clientSuffix, () => {
-		const url1 = new URL( 'http://localhost:3000' )
-		const url2 = new URL( 'http://localhost:3000' )
-		const url3 = new URL( 'http://localhost:3001' )
-
-		expect(
-			isExternalUrl( url1, url2 )
-		).toBe( false )
-
-		expect(
-			isExternalUrl( url1, url3 )
-		).toBe( true )
-	} )
-
-
-	it( 'checks URL validity' + clientSuffix, () => {
-		expect(
-			isExternalUrl( 'invalid url', new URL( 'http://localhost:3000' ) )
-		).toBe( false )
-	} )
-	
-} )
+isExternalUrlUnitTests()
 
 
 describe( 'isAbsoluteUrl', () => {
