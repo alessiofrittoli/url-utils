e83b6285128fca552196bcfa4ee89af50836ffe4
Alessio Frittoli <35973186+alessiofrittoli@users.noreply.github.com>
2025-05-02 19:58:07 +0200

    chore: minor improvements (#21)

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,14 +1,15 @@
 import { defineConfig } from 'tsup'
 
+const enableSourcemap = process.env.NODE_ENV !== 'production'
+
 export default defineConfig( {
 	entry		: [ 'src/**/*.ts' ],
 	format		: [ 'cjs', 'esm' ],
 	dts			: true,
-	splitting	: false,
+	splitting	: true,
 	shims		: true,
 	skipNodeModulesBundle: true,
 	clean		: true,
-	treeshake	: true,
 	minify		: true,
-	sourcemap	: true,
+	sourcemap	: enableSourcemap,
 } )
